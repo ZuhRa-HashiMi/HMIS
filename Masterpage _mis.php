717ff68cbcc4a0a5a01172b5c6a149cb00774778
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-08-22 18:35:41 +0430

    add

--- Masterpage _mis.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("header.php");?>
-
-
-
-
-
-<?php require_once("footer_mis.php");?>
--- Masterpage_website.php
+++
@@ -1,6 +0,0 @@
-<?php require_once("header.php");?>
-
-
-
-<?php require_once("footer_website.php");?>
-
